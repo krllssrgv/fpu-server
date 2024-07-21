1814d9ee3bd0d88e0ac2e71d04ab1fae12fde392
Kirill Savin <krllssrgv@gmail.com>
2024-07-21 18:30:10 +0300

    Init commit

---
+++ app.py
@@ -0,0 +1,403 @@
+# This Python file uses the following encoding: utf-8
+import os
+import sys
+
+from flask import Flask, request, send_from_directory, Blueprint, make_response
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, upgrade, init, migrate
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, get_jwt, set_access_cookies, unset_jwt_cookies
+from flask_cors import CORS
+from flask_restx import Api, Resource, Namespace, fields
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from datetime import timedelta
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from check_user import send_email, create_code
+from data import Data
+
+from config import SECRET_KEY, JWT_SECRET_KEY, SQLALCHEMY_DATABASE_URI
+
+
+
+# App
+app = Flask(__name__, static_folder='build', static_url_path='')
+
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
+app.config['JWT_TOKEN_LOCATION'] = ['cookies']
+app.config['JWT_COOKIE_CSRF_PROTECT'] = True
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=15)
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access']
+app.config['JWT_COOKIE_SAMESITE'] = 'None'
+app.config['JWT_COOKIE_SECURE'] = True
+app.config['JWT_COOKIE_CSRF_PROTECT'] = False
+
+
+# CORS
+CORS(app, supports_credentials=True)
+
+# JWT
+jwt = JWTManager(app)
+
+# API
+api_bp = Blueprint('API', __name__, url_prefix='/api')
+api = Api(api_bp)
+user_api = Namespace('user')
+act_api = Namespace('act')
+
+# DataBase
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+@app.cli.command("init-db")
+def init_db():
+    init()
+    migrate()
+    upgrade()
+
+
+class users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(30), nullable=False)
+    name = db.Column(db.String(40), nullable=False)
+    surname = db.Column(db.String(40), nullable=False)
+
+    confirmed = db.Column(db.Boolean, default=False, nullable=False)
+    check_code = db.Column(db.String(10), default='', nullable=False)
+    
+    current_week = db.Column(db.Integer, nullable=False, default=0)
+    day_one = db.Column(db.Boolean, nullable=False, default=False)
+    day_two = db.Column(db.Boolean, nullable=False, default=False)
+    day_three = db.Column(db.Boolean, nullable=False, default=False)
+    success = db.Column(db.String(2), nullable=False, default='')
+
+    def __repr__(self):
+        return '<users %r>' % self.id
+
+
+# Models
+user_login_model = user_api.model('user_login', {
+    'email': fields.String(required=True, description='Email'),
+    'password': fields.String(required=True, description='Password')
+})
+
+user_confirm_model = user_api.model('user_login', {
+    'code': fields.String(required=True, description='Code'),
+})
+
+
+BLACKLIST = set()
+
+@jwt.token_in_blocklist_loader
+def check_if_token_in_blacklist(jwt_header, jwt_payload):
+    jti = jwt_payload['jti']
+    return jti in BLACKLIST
+
+
+
+# User API
+
+@user_api.route('/register')
+class Register(Resource):
+    def post(self):
+        data = request.json
+        check = True
+        result = {}
+
+        if (users.query.filter(users.email == data['email']).all()):
+            check = False
+            result['email'] = 'Пользователь уже существует'
+
+        if (len(data['password']) < 6):
+            check = False
+            result['password'] = 'Минимальная длина 6 символов'
+
+        if (data['password'] != data['repeatedPassword']):
+            check = False
+            result['repeatedPassword'] = 'Пароли не совпадают'
+
+        if check:
+            code = create_code()
+            new_user = users(
+                email=str(data['email']),
+                password=generate_password_hash(str(data['password'])),
+                name=str(data['name']),
+                surname=str(data['surname']),
+                check_code = code
+            )
+            try:
+                db.session.add(new_user)
+                db.session.commit()
+                print(send_email(str(data['email']), code))
+                return '', 204
+            except:
+                return '', 500
+        else:
+            return result, 400
+
+
+@user_api.route('/login')
+class Login(Resource):
+    @user_api.expect(user_login_model)
+    def post(self):
+        data = request.json
+        user = users.query.filter_by(email=data['email']).first()
+        if user:
+            if check_password_hash(user.password, data['password']):
+                access_token = create_access_token(identity=user.id)
+                response = make_response()
+                set_access_cookies(response, access_token)
+                return response
+            else:
+                return {'password': 'Неправильный пароль'}, 401
+        else:
+            return {'email': 'Нет такого пользователя'}, 401
+
+
+@user_api.route('/logout')
+class Logout(Resource):
+    @jwt_required(optional=True)
+    def post(self):
+        user = db.session.get(users, get_jwt_identity())
+
+        if (user):
+            jti = get_jwt()['jti']
+            BLACKLIST.add(jti)
+            response = make_response()
+            unset_jwt_cookies(response)
+            return response
+        else:
+            return '', 401
+    
+
+@user_api.route('/check_login')
+class CheckLogin(Resource):
+    @jwt_required(optional=True)
+    def get(self):
+        user = db.session.get(users, get_jwt_identity())
+
+        if (user):
+            return '', 204
+        else:
+            return '', 401
+        
+
+@user_api.route('/confirm_email')
+class ConfirmEmail(Resource):
+    @jwt_required(optional=True)
+    @user_api.expect(user_confirm_model)
+    def post(self):
+        user = db.session.get(users, get_jwt_identity())
+
+        if (user):
+            if (not user.confirmed):
+                code = request.json['code']
+                if (code == user.check_code):
+                    user.check_code = ''
+                    user.confirmed = True
+                    try:
+                        db.session.commit()
+                        return '', 204
+                    except:
+                        return '', 500
+                else:
+                    return {'error': 'Неправильный код'}, 400
+            else:
+                return {'error': 'Почта уже подтверждена'}, 400
+        else:
+            return '', 401
+        
+
+@user_api.route('/user')
+class UserRequests(Resource):
+    @jwt_required(optional=True)
+    def get(self):
+        user = db.session.get(users, get_jwt_identity())
+
+        if (user):
+            progress = ''
+            if (user.current_week < 14):
+                progress = int((round(user.current_week / 14, 2)) * 100)
+            else:
+                progress = 100
+
+            return {
+                "name": user.name,
+                "surname": user.surname,
+                "email": user.email,
+                "confirmed": user.confirmed,
+                "progress": progress
+            }
+        else:
+            return '', 401
+        
+
+@user_api.route('/remove')
+class RemoveUser(Resource):
+    @jwt_required(optional=True)
+    def get(self):
+        user = db.session.get(users, get_jwt_identity())
+
+        if (user):
+            try:
+                db.session.delete(user)
+                db.session.commit()
+                jti = get_jwt()['jti']
+                BLACKLIST.add(jti)
+                response = make_response()
+                unset_jwt_cookies(response)
+                return response
+            except:
+                return '', 500
+        else:
+            return '', 401
+
+
+# Act API
+
+@act_api.route('/get_acts')
+class GetActs(Resource):
+    @jwt_required(optional=True)
+    def get(self):
+        user = db.session.get(users, get_jwt_identity())
+
+        if (user):
+            if (user.current_week < 14):
+                user_data = Data.get_week(user.current_week)
+                days = [
+                    {
+                        'number': 1,
+                        'done': user.day_one,
+                        'acts': user_data[1]
+                    },
+                    {
+                        'number': 2,
+                        'done': user.day_two,
+                        'acts': user_data[2]
+                    },
+                    {
+                        'number': 3,
+                        'done': user.day_three,
+                        'acts': user_data[3]
+                    }
+                ]
+                
+                return {
+                    'name': user.name,
+                    'progress': int((round(user.current_week / 14, 2)) * 100),
+                    'success': user.success,
+                    'current_week': user.current_week,
+                    'types': user_data[0],
+                    'days': days,
+                    'finish': False
+                }
+            else:
+                return {
+                    'name': user.name,
+                    'progress': 100,
+                    'finish': True
+                }
+        else:
+            return '', 401
+
+
+@act_api.route('/set_day_as_done')
+class DoneDay(Resource):
+    @jwt_required(optional=True)
+    def post(self):
+        user = db.session.get(users, get_jwt_identity())
+
+        if (user):
+            data = request.json
+            if ('set_day' in data):
+                if (data['set_day'] == 1):
+                    user.day_one = True
+                elif (data['set_day'] == 2):
+                    user.day_two = True
+                elif (data['set_day'] == 3):
+                    user.day_three = True
+                    
+                if (user.day_one and user.day_two and user.day_three):
+                    user.success = '0'
+
+                try:
+                    db.session.commit()
+                    return '', 200
+                except:
+                    return '', 500
+            else:
+                    return '', 400
+        else:
+            return '', 401
+
+
+@act_api.route('/send_result')
+class Result(Resource):
+    @jwt_required(optional=True)
+    def post(self):
+        user = db.session.get(users, get_jwt_identity())
+
+        if (user):
+            data = request.json
+            if ('success' in data):
+                if (data['success']):
+                    user.success = '1'
+                else:
+                    user.success = '-1'
+                    
+                try:
+                    db.session.commit()
+                    return {'success': user.success}, 200
+                except:
+                    return '', 500 
+            else:
+                return '', 400
+        else:
+            return '', 401 
+
+
+# Register
+api.add_namespace(act_api, path='/act')
+api.add_namespace(user_api, path='/user')
+app.register_blueprint(api_bp)
+
+
+# Serve
+@app.route('/', defaults={'path': ''})
+@app.route('/<path>')
+def serve(path):
+    if (path != "") and (os.path.exists(os.path.join(app.static_folder, path))):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+
+
+# BackgroundScheduler
+def update_weeks():
+    with app.app_context():
+        all_users = users.query.all()
+        for user in all_users:
+            user.day_one = False
+            user.day_two = False
+            user.day_three = False
+
+            if (user.success == '1'):
+                user.current_week += 1
+
+            user.success = ''
+        db.session.commit()
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(update_weeks, 'cron', day_of_week='mon', hour=1, minute=0)
+scheduler.start()
+
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ data.py
@@ -0,0 +1,100 @@
+from copy import copy
+
+
+class Data:
+    straight = 'straight'
+    narrow = 'narrow'
+    wide = 'wide'
+    reverse = 'reverse'
+
+    acts = [
+        [
+            [straight, reverse, narrow, reverse, straight],
+            [5, 4, 5, 4, 3],
+            [6, 5, 5, 6, 4],
+            [5, 6, 6, 5, 5]
+        ],
+        [
+            [straight, reverse, narrow, reverse, straight],
+            [6, 5, 4, 6, 5],
+            [8, 5, 5, 4, 4],
+            [5, 9, 5, 5, 5]
+        ],
+        [
+            [straight, reverse, narrow, reverse, wide],
+            [8, 6, 5, 6, 2],
+            [8, 7, 6, 5, 2],
+            [10, 6, 7, 8, 2]
+        ],
+        [
+            [straight, reverse, narrow, reverse, wide],
+            [8, 10, 6, 6, 2],
+            [11, 6, 8, 6, 2],
+            [10, 10, 10, 6, 2]
+        ],
+        [
+            [straight, reverse, straight, reverse, straight],
+            [10, 9, 10, 7, 6],
+            [11, 10, 9, 7, 5],
+            [14, 12, 11, 9, 0]
+        ],
+        [
+            [straight, reverse, narrow, reverse, straight],
+            [11, 12, 11, 10, 10],
+            [15, 12, 10, 9, 9],
+            [17, 15, 12]
+        ],
+        [
+            [straight, reverse, narrow, reverse, straight],
+            [14, 16, 10, 10, 10],
+            [15, 15, 10, 10, 10],
+            [14, 13, 12, 11, 10]
+        ],
+        [
+            [straight, reverse, straight, reverse, reverse],
+            [12, 10, 9, 9, 8],
+            [12, 10, 10, 9, 9],
+            [13, 10, 10, 9, 8]
+        ],
+        [
+            [straight, reverse, straight, reverse, reverse],
+            [15, 11, 10, 9, 8],
+            [15, 12, 11, 9, 8],
+            [14, 13, 12, 11, 9]
+        ],
+        [
+            [straight, reverse, straight, reverse, reverse],
+            [15, 14, 10, 12, 10],
+            [15, 14, 11, 13, 10],
+            [14, 14, 14, 12, 11]
+        ],
+        [
+            [straight, reverse, straight, reverse, reverse],
+            [17, 10, 16, 10, 14],
+            [14, 19, 11, 13, 9],
+            [19, 13, 14, 13, 11]
+        ],
+        [
+            [straight, reverse, straight, reverse, straight],
+            [22, 13, 11, 10, 888],
+            [24, 10, 11, 13, 888],
+            [26, 10, 11, 10, 888]
+        ],
+        [
+            [straight, reverse, straight, reverse, straight],
+            [30, 15, 10, 15, 888],
+            [33, 16, 11, 13, 888],
+            [35, 10, 11, 10, 888]
+        ],
+        [
+            [straight, reverse, straight],
+            [30, 22, 888],
+            [34, 20, 888],
+            [19, 13, 888]
+        ]    
+    ]
+
+
+    @classmethod
+    def get_week(cls, week):
+        return cls.acts[week]
